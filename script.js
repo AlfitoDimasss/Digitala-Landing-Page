3550fac558fbfabd2be9503e71db9559f00030b1
AlfitoDimasss <dimasalfito89@gmail.com>
2023-05-20 11:32:52 +0700

    add js

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
-// Smooth Scroll
-const links = document.querySelectorAll("nav ul li a");
-
-for (const link of links) {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const href = this.getAttribute("href");
-    const offsetTop = document.querySelector(href).offsetTop;
-    scroll({
-      top: offsetTop,
-      behavior: "smooth"
-    });
-  });
-}
-
-// Collapse Navbar
-const hamburger = document.querySelector(".hamburger-button");
-hamburger.addEventListener('click', function () {
-  const links = document.querySelector('.nav-link');
-  const display = links.style.display;
-  if (display == '' || display == 'none') {
-    links.style.display = 'block';
-  } else {
-    links.style.display = 'none';
-  }
-});
-
-
+const hamburgerButton = document.getElementById('hamburgerButton');
+const navLink = document.getElementsByClassName('nav-bar__link')[0];
+hamburgerButton.addEventListener('click', function () {
+  navLink.classList.toggle('open');
+})
